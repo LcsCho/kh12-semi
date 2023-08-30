d137c128ecd6fc8a37e1bebe4282ff15460b4095
yejishin95 <yeji2336@naver.com>
2023-08-30 10:53:24 +0900

    주석달기

--- baseball/src/main/java/com/kh/baseball/dto/HomerunBallDto.java
+++ baseball/src/main/java/com/kh/baseball/dto/HomerunBallDto.java
@@ -4,6 +4,7 @@ public class HomerunBallDto {
 
 	private void sysout() {
 		// TODO Auto-generated method stub
+		//주석달기 -예지-
 System.out.println();
 	}
 }
--- baseball/src/main/java/com/kh/baseball/dto/JunheeDto.java
+++ baseball/src/main/java/com/kh/baseball/dto/JunheeDto.java
@@ -2,4 +2,5 @@ package com.kh.baseball.dto;
 
 public class JunheeDto {
 private String love;
+//주석달기 예지
 }
