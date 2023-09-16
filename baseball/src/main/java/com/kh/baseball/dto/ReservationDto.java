4f13a19cece7a0aabc0541b64c6736add75f1a46
LcsCho <chojunhee98@gmail.com>
2023-09-16 13:42:22 +0900

    제거

--- baseball/src/main/java/com/kh/baseball/dto/ReservationDto.java
+++
@@ -1,5 +0,0 @@
-package com.kh.baseball.dto;
-
-public class ReservationDto {
-
-}
--- baseball/src/main/java/com/kh/baseball/dto/StadiumDto.java
+++
@@ -1,9 +0,0 @@
-package com.kh.baseball.dto;
-
-import lombok.Data;
-
-@Data
-public class StadiumDto {
-	private String stadium_id;
-	private String stadium_name;
-}
