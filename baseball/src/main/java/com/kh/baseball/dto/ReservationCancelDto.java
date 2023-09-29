fa2a0a4a95ed022ef019d0e73a3e26f9f5dddb5d
Bokman0201 <jeonghs24@nate.com>
2023-09-29 13:57:27 +0900

    save

--- baseball/src/main/java/com/kh/baseball/dto/ReservationCancelDto.java
+++ baseball/src/main/java/com/kh/baseball/dto/ReservationCancelDto.java
@@ -6,17 +6,9 @@ import lombok.Data;
 
 @Data
 public class ReservationCancelDto {
-	private int reservationNo;
-	private int matchNo;
-	private int seatNo;
-	private String homeTeam, awayTeam;
-	private String memberId;
-	private String stadiumName;
-	private Date reservationDate;
-	private int seatAreaPrice;
-	private int stadiumNo;
-	private String seatAreaZone;
-	private int seatRow, seatCol;
-	private int seatAreaNo;
-	private Date matchDate;
+	
+	private int reservationNo , seatNo, reservationCancelNo;
+	private Date reservationCancelDate;
+	
+	
 }
